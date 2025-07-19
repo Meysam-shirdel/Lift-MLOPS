dfb6aa1619930285c974f06e7b1932e4c71d8a58
unknown <meysam.shirdel2@gmail.com>
2025-07-19 01:37:16 -0400

    scr2

--- src/logger.py
+++ src/logger.py
@@ -6,3 +6,5 @@ import logger
 LOGS_DIR = Path("logs")
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
 LOG_FILE = LOGS_DIR / f'{datetime.now().strftime("%Y- %m- %d   %H: %M: %S")}.log'
+
+print(LOG_FILE)
